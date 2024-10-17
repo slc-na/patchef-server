a2f880ce8510add07180ddbfb89a19bc8ea86a39
6ixB <124480418+6ixB@users.noreply.github.com>
2024-10-17 15:47:40 +0700

    feat: integrate Prisma ORM into Nest app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,21 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER, HttpAdapterHost } from '@nestjs/core';
+import { PrismaClientExceptionFilter, PrismaModule } from 'nestjs-prisma';
 
-@Module({})
+@Module({
+  imports: [
+    PrismaModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  providers: [
+    {
+      provide: APP_FILTER,
+      inject: [HttpAdapterHost],
+      useFactory: ({ httpAdapter }: HttpAdapterHost) => {
+        return new PrismaClientExceptionFilter(httpAdapter);
+      },
+    },
+  ],
+})
 export class AppModule {}

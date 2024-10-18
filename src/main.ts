e7cacc2cdbc1607ba0dd4f4df4ea0c15e67e7397
6ixB <124480418+6ixB@users.noreply.github.com>
2024-10-18 12:11:07 +0700

    docs: add API documentation using Swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -10,6 +11,16 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('PatChef')
+    .setDescription('The PatChef API description')
+    .setVersion('0.0.1')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(3000);
 }
 

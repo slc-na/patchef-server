c3cb7cfaef6df22aa84e24498f1b2cbe8e4464ac
6ixB <124480418+6ixB@users.noreply.github.com>
2024-10-18 21:42:00 +0700

    docs: add custom site title and favicon to api docs

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import {
 } from '@nestjs/platform-fastify';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import fastifyStatic from '@fastify/static';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -12,16 +14,30 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
 
+  // Static Files Configuration
+  app
+    .getHttpAdapter()
+    .getInstance()
+    .register(fastifyStatic, {
+      root: join(__dirname, '..', 'public'),
+      prefix: '/public/',
+    });
+
+  // Swagger Configuration
   const config = new DocumentBuilder()
     .setTitle('PatChef')
     .setDescription('The PatChef API description')
     .setVersion('0.0.1')
     .build();
 
+  // Swagger Document Build Setup
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('api', app, documentFactory, {
+    customSiteTitle: 'PatChef API',
+    customfavIcon: './public/square-terminal.svg',
+  });
 
-  await app.listen(3000);
+  await app.listen({ port: 3000 });
 }
 
 bootstrap();
